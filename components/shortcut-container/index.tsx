b4e8cea2e9ea77a3fb3949b2635a26f9e0f303ba
stoicperson <81851068+stoicperson@users.noreply.github.com>
2023-02-24 19:33:05 +0900

    Update index.tsx

--- components/shortcut-container/index.tsx
+++ components/shortcut-container/index.tsx
@@ -1,19 +1,29 @@
+import styles from "./index.module.scss";
 import { motion } from "framer-motion";
 
-const container = {};
+const container = {
+  hidden: { y: 20, x: -20, opacity: 0 },
+  visible: { y: 0, x: 0, opacity: 1 },
+};
 
 const item = {
-  hidden: { y: 20, opacity: 0 },
-  visible: { y: 0, opacity: 1 },
+  hidden: { y: 20, x: -20, opacity: 0 },
+  visible: { y: 0, x: 0, opacity: 1 },
 };
 
 const ShortcutContainer = () => {
   return (
-    <motion.div animate="visible" variants={container}>
-      <motion.div variants={item}></motion.div>
-      <motion.div variants={item}></motion.div>
-      <motion.div variants={item}></motion.div>
-      <motion.div variants={item}></motion.div>
+    <motion.div
+      className={styles.container}
+      initial="hidden"
+      animate="visible"
+      variants={container}
+    >
+      {["ss", "cc", "aa", "bb"].map((el) => (
+        <motion.div className={styles.item} key={el} variants={item}>
+          el
+        </motion.div>
+      ))}
     </motion.div>
   );
 };
