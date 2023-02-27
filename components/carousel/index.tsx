0ff8be71b137b6cc7bf556d4da6aefa4266c1f7a
stoicperson <81851068+stoicperson@users.noreply.github.com>
2023-02-27 20:04:19 +0900

    Update carousel tsx

--- components/carousel/index.tsx
+++ components/carousel/index.tsx
@@ -1,13 +1,62 @@
+import styles from "./index.module.scss";
+import {
+  motion,
+  useAnimationFrame,
+  useMotionValue,
+  useScroll,
+  useSpring,
+  useTransform,
+  useVelocity,
+} from "framer-motion";
+import { useRef } from "react";
+
+const wrap = (min: number, max: number, v: number) => {
+  const rangeSize = max - min;
+  return ((((v - min) % rangeSize) + rangeSize) % rangeSize) + min;
+};
+
 interface IProps {
-  children: JSX.Element | JSX.Element[];
+  baseVelocity: number;
 }
 
-const Carousel = ({ children }: IProps) => {
+const Carousel = ({ baseVelocity }: IProps) => {
+  const baseX = useMotionValue(0);
+  const { scrollY } = useScroll();
+  const scrollVelocity = useVelocity(scrollY);
+  const smoothVelocity = useSpring(scrollVelocity, {
+    damping: 40,
+    stiffness: 500,
+  });
+  const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 2.5], {
+    clamp: false,
+  });
+  const x = useTransform(baseX, (v) => `${wrap(-50, 0, v)}%`);
+  const directionFactor = useRef<number>(1);
+  useAnimationFrame((t, delta) => {
+    let moveBy = baseVelocity * (delta / 1000);
+
+    if (velocityFactor.get() < 0) {
+      directionFactor.current = -1;
+    } else if (velocityFactor.get() > 0) {
+      directionFactor.current = 1;
+    }
+
+    moveBy += directionFactor.current * moveBy * velocityFactor.get();
+
+    baseX.set(baseX.get() + moveBy);
+  });
   return (
-    <div>
-      <div>Title</div>
-      <div>{children}</div>
-      <div>See more</div>
+    <div className={styles.container}>
+      <motion.div className={styles.item} style={{ x }}>
+        <div>a</div>
+        <div>b</div>
+        <div>c</div>
+        <div>d</div>
+        <div>a</div>
+        <div>b</div>
+        <div>c</div>
+        <div>d</div>
+      </motion.div>
     </div>
   );
 };
