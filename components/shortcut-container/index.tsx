394696505c0ad1fd53c0dd8a58bed2d18e4f6ef5
stoicperson <81851068+stoicperson@users.noreply.github.com>
2023-03-03 20:18:26 +0900

    Update shortcut-container.tsx

--- components/shortcut-container/index.tsx
+++ components/shortcut-container/index.tsx
@@ -1,6 +1,6 @@
 import styles from "./index.module.scss";
 import Link from "next/link";
-import { easeInOut, motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 const container = {
   visible: {
@@ -11,18 +11,6 @@ const container = {
 };
 const item = {
   hidden: { scale: 0.9, opacity: 0 },
-  hover_one: {
-    scale: 0.976,
-    transition: { duration: 0.25, ease: "easeInOut" },
-  },
-  hover_two: {
-    scale: 0.95,
-    transition: { duration: 0.25, ease: "easeInOut" },
-  },
-  view: {
-    scale: 1,
-    transition: { duration: 0.25, ease: "easeInOut" },
-  },
   visible: { scale: 1, opacity: 1, transition: { duration: 0.6 } },
 };
 
@@ -30,40 +18,64 @@ const ShortcutContainer = () => {
   return (
     <motion.div
       className={styles.container}
+      initial="hidden"
       animate="visible"
       variants={container}
     >
       <Link href="/about">
-        <Shortcut text="about" type="one" />
+        <Shortcut>
+          <h3>About</h3>
+        </Shortcut>
       </Link>
       <Link href="/project">
-        <Shortcut text="project" type="one" />
+        <Shortcut>
+          <h3>Project</h3>
+        </Shortcut>
       </Link>
       <Link href="/blog">
-        <Shortcut text="blog" type="two" />
+        <Shortcut>
+          <h3>blog</h3>
+        </Shortcut>
       </Link>
       <a>
-        <Shortcut text="contact" type="two" />
+        <Shortcut>
+          <Contact />
+        </Shortcut>
       </a>
     </motion.div>
   );
 };
 
 interface IPShortcut {
-  text: string;
-  type: string;
+  children: JSX.Element;
 }
 
-const Shortcut = ({ text, type }: IPShortcut) => {
+const Shortcut = ({ children }: IPShortcut) => {
   return (
-    <motion.div
-      whileHover={`hover_${type}`}
-      whileInView="view"
-      className={styles.item}
-      variants={item}
-    >
-      <h3>{text}</h3>
+    <motion.div className={styles.item} variants={item}>
+      {children}
     </motion.div>
+  );
+};
+
+const About = () => {};
+
+const Contact = () => {
+  return (
+    <div className={styles.contact}>
+      <svg
+        width="900"
+        height="900"
+        viewBox="0 0 900 900"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M857.366 509.858C857.521 506.536 856.396 503.359 854.346 500.915C853.109 499.442 851.55 498.247 849.7 497.43L66.8198 150.983C58.8123 147.447 49.4712 148.795 42.7829 154.477C36.1033 160.151 33.2492 169.146 35.4433 177.634L91.7129 394.857L607.092 491.144L105.656 448.675L175.756 719.279C176.61 722.608 178.22 725.675 180.372 728.241C182.209 730.43 184.427 732.263 186.969 733.646C192.528 736.655 199.096 737.241 205.099 735.248L848.537 521.497C853.608 519.824 857.123 515.19 857.366 509.858Z"
+          fill="black"
+        />
+      </svg>
+    </div>
   );
 };
 
